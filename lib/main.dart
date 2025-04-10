f0d54ea5296196c62626c3032b37a0914ea2ee26
ibrahim <Ibrahim.Ali@totplatform.com>
2025-04-10 17:46:32 +0200

    Refactor DebugPaintSizeEnabled to use Cupertino widgets for layout; update main.dart to reflect changes

--- lib/main.dart
+++ lib/main.dart
@@ -2,4 +2,4 @@ import 'package:flutter/material.dart';
 import 'package:widget_flutter/widget/debug_paint_size_enabled.dart'
     show DebugPaintSizeEnabled;
 
-void main() => runApp(MaterialApp(home: DebugPaintSizeEnabled()));
+void main() => runApp(DebugPaintSizeEnabled());
--- lib/widget/debug_paint_size_enabled.dart
+++ lib/widget/debug_paint_size_enabled.dart
@@ -1,3 +1,10 @@
+import 'package:flutter/cupertino.dart'
+    show
+        CupertinoApp,
+        CupertinoColors,
+        CupertinoNavigationBar,
+        CupertinoPageScaffold,
+        CupertinoThemeData;
 import 'package:flutter/material.dart';
 
 class DebugPaintSizeEnabled extends StatelessWidget {
@@ -5,6 +12,43 @@ class DebugPaintSizeEnabled extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text('data')));
+    return CupertinoApp(
+      title: 'Flutter layout demo',
+      theme: CupertinoThemeData(
+        brightness: Brightness.light,
+        primaryColor: CupertinoColors.systemBlue,
+      ),
+      home: CupertinoPageScaffold(
+        navigationBar: const CupertinoNavigationBar(
+          backgroundColor: CupertinoColors.systemGrey,
+          middle: Text('Cupertino Navigation Bar'),
+        ),
+
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [Text('Hello world')],
+          ),
+        ),
+      ),
+    );
+    const String appTitle = 'flutter debug paint size enabled';
+    return MaterialApp(
+      title: appTitle,
+      home: Scaffold(
+        appBar: AppBar(title: Text(appTitle)),
+        body: Center(child: Text('Hello world')),
+      ),
+    );
+    return Container(
+      decoration: BoxDecoration(color: Colors.white),
+      child: Center(
+        child: Text(
+          'data',
+          textDirection: TextDirection.ltr,
+          style: TextStyle(fontSize: 32, color: Colors.black87),
+        ),
+      ),
+    );
   }
 }
