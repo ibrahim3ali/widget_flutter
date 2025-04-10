44509ace1b603d6db7d90f6f22090a6ad46ff0c6
ibrahim <Ibrahim.Ali@totplatform.com>
2025-04-10 13:11:52 +0200

    first commit

---
+++ lib/widget/absorb_pointer.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AbsorbPointer extends StatelessWidget {
+  const AbsorbPointer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
